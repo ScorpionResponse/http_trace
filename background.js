06f67a0463b6c2ba5e8d542321712cab609a6375
Paul Moss <moss.paul@gmail.com>
2013-05-02 17:46:09 -0400

    Reworked to use webRequest instead of debugger permissions

--- background.js
+++ background.js
@@ -1,12 +1,4 @@
 
-callback = function(details) {
-    do_alert(details);
-};
-
-filters = { urls: ["<all_urls>"]}
-
 chrome.browserAction.onClicked.addListener(function(tab) {
-  alert("clicked");
-  chrome.webRequest.onSendHeaders.addListener(callback, filters);
   chrome.windows.create({url: "headers.html?" + tab.id, type: "popup", width: 800, height: 600});
 });
--- headers.js
+++ headers.js
@@ -1,65 +1,57 @@
 
-function do_alert(info) {
-    alert(info);
-    $("#container").append(JSON.stringify(info));
+var tabId = parseInt(window.location.search.substring(1));
+
+callback = function(details) {
+    handleEvent(details);
+};
+
+filters = { urls: ["<all_urls>"], tabId: tabId }
+
+chrome.webRequest.onBeforeRequest.addListener(callback, filters, ['requestBody']);
+chrome.webRequest.onSendHeaders.addListener(callback, filters, ['requestHeaders']);
+chrome.webRequest.onBeforeRedirect.addListener(callback, filters, ['responseHeaders']);
+chrome.webRequest.onCompleted.addListener(callback, filters, ['responseHeaders']);
+chrome.webRequest.onErrorOccurred.addListener(callback, filters);
+
+function handleEvent(details) {
+    //$("#container").append("<!-- DEBUG: " + JSON.stringify(details) + "--> \n");
+    var addressDiv = $('div.address[id="req-' + details.requestId + '"]');
+    if (addressDiv.length == 0) {
+        var addressDiv = $('<div>').addClass("address").attr("id", "req-" + details.requestId);
+        $("#container").append(addressDiv);
+        $('<div>').addClass("url").text(details.url).appendTo(addressDiv);
+    }
+
+    if (details.requestHeaders) {
+        $('<div>').addClass("request").text('\n' + details.method + ' ' + details.url).appendTo(addressDiv);
+        addressDiv.children().last().append(formatHeaders(details.requestHeaders));
+    } else if (details.redirectUrl) {
+        $('<div>').addClass("redirect").text('\n' + details.statusLine + "\n Redirect to: " + details.redirectUrl).appendTo(addressDiv);
+        addressDiv.children().last().append(formatHeaders(details.responseHeaders));
+    } else if (details.responseHeaders) {
+        $('<div>').addClass("response").text('\n' + details.statusLine).appendTo(addressDiv);
+        addressDiv.children().last().append(formatHeaders(details.responseHeaders));
+    }
+
+    if (details.requestBody) {
+        addressDiv.children().last().append(formatPost(details.requestBody.formData));
+    }
 }
 
-function onEvent(debuggeeId, message, params) {
-  if (tabId != debuggeeId.tabId) {
-    return;
-  }
-
-  if (message == "Network.requestWillBeSent") {
-    var requestDiv = $('div.address[id="req-' + params.requestId + '"]');
-    if (requestDiv.length == 0) {
-      var requestDiv = $('<div>').addClass("address").attr("id", "req-" + params.requestId);
-      $('<div>').addClass("url").text(params.request.url).appendTo(requestDiv);
+function formatPost(postData) {
+    var text = "";
+    for (name in postData) {
+        text += name + ": " + postData[name] + "\n";
     }
-
-    if (params.redirectResponse) {
-      appendResponse(params.requestId, params.redirectResponse);
-    }
-
-    $('<div>').addClass("request").text("\n" + params.request.method + " " +
-        parseURL(params.request.url).path + " HTTP/1.1").appendTo(requestDiv);
-
-    if (params.request.method === 'POST') {
-      $('<div>').addClass("post").text("\n" + params.request.postData).appendTo(requestDiv);
-    }
-
-    $("#container").append(requestDiv);
-  } else if (message == "Network.responseReceived") {
-    appendResponse(params.requestId, params.response);
-  }
-}
-
-function appendResponse(requestId, response) {
-  var requestDiv = $('div.address[id="req-' + requestId + '"]');
-  requestDiv.children().last().append(formatHeaders(response.requestHeaders));
-
-  $('<div>').addClass("response").text("\nHTTP/1.1 " + response.status + " " +
-          response.statusText).appendTo(requestDiv);
-  requestDiv.children().last().append(formatHeaders(response.headers));
+    var div = $('<div>').addClass("post").text(text);
+    return div;
 }
 
 function formatHeaders(headers) {
-  var text = "";
-  for (name in headers)
-    text += name + ": " + headers[name] + "\n";
-  var div = $('<div>').addClass("headers").text(text);
-  return div;
-}
-
-function parseURL(url) {
-  var result = {};
-  var match = url.match(
-      /^([^:]+):\/\/([^\/:]*)(?::([\d]+))?(?:(\/[^#]*)(?:#(.*))?)?$/i);
-  if (!match)
-    return result;
-  result.scheme = match[1].toLowerCase();
-  result.host = match[2];
-  result.port = match[3];
-  result.path = match[4] || "/";
-  result.fragment = match[5];
-  return result;
+    var text = "";
+    for (i in headers) {
+        text += headers[i].name + ": " + headers[i].value + "\n";
+    }
+    var div = $('<div>').addClass("headers").text(text);
+    return div;
 }
